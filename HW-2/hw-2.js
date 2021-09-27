9bdc7fcc503a2b6a8de113941be2dd9fc1819da0
Marharyta kulinich <margaret.kulinich@gmail.com>
2021-09-27 12:38:22 +0300

    modified veriable declaration

--- HW-2/hw-2.js
+++ HW-2/hw-2.js
@@ -1,13 +1,13 @@
-let n = +prompt("Введіть число", 0);
+const n = +prompt("Введіть число", 0);
 
 if (!isNaN(n) && (n % 1 === 0)) {
-    let m = +prompt("Введіть число", 0);
+    const m = +prompt("Введіть число", 0);
 
     if (!isNaN(m) && (n % 1 === 0)) {
         const skipEvenNumbers = confirm("Чи потрібно пропускати парні числа?");
         let sum = 0;
-        let from = (n <= m) ? n : m;
-        let to = (n <= m) ? m : n;
+        const from = (n <= m) ? n : m;
+        const to = (n <= m) ? m : n;
 
         for (i = from; i <= to; i++) {
             if (skipEvenNumbers && (i % 2 === 0)) {
